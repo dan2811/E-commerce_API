2f973bf39ec7be7fc6589c3ee6b685446d8b887b
dan2811 <djordandrums@gmail.com>
2022-01-07 17:14:27 +0000

    add CRUD user routes and JWT

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const User = require('../models/User');
 const CryptoJS = require("crypto-js");
+const { JsonWebTokenError } = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 
 //REGISTER
 router.post("/register", async (req, res)=>{
@@ -21,5 +23,36 @@ router.post("/register", async (req, res)=>{
         res.status(500).json(err);
     }
 });
+
+
+//LOGIN
+
+router.post("/login", async (req, res)=>{
+    try {
+        const user = await User.findOne({username: req.body.username });
+        !user && res.status(401).json("Wrong credentials!");
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASS_SECRET
+            );
+            const decryptedPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+            decryptedPassword !== req.body.password &&
+            res.status(401).json("Wrong credentials!");
+
+                const accessToken = jwt.sign({
+                    id:user._id,
+                    isAdmin: user.isAdmin,
+                }, process.env.JWT_SECRET,
+                {expiresIn:"3d"}
+                );   
+
+            const {password, ...others} = user._doc;
+            res.status(200).json({...others, accessToken});
+
+    } catch(err) {
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
